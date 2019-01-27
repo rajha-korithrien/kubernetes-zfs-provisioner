9222e7051db3a599c4ba80d7a52fe4b8eed7743c
Max Planck <mplanck@icasa.nmt.edu>
2019-01-27 09:50:48 -0700

    Added two kinds of Dockerfile support
    *One to deploy via a regualr docker container where the host must supply zfs
    *One to deploy as a RancherOS system service

---
+++ deploy/rancher/provisioner-entry.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#Check that we are on Rancher and can use ros
+if [ ! -e /usr/bin/system-docker ]; then
+    echo "This image can only be used on RancherOS when it has access to system-docker."
+    exit 1
+fi
+
+#Make our device tree be in the correct place
+mount --rbind /host/dev /dev > /dev/null 2>&1
+
+#Now we execute the zfs-provisioner
+echo "Starting zfs-provisioner via $@"
+echo "Naming zfs-provisioner: $ZFS_PROVISIONER_NAME"
+exec "$@"
